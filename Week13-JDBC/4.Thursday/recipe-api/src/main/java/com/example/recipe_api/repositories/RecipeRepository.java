305238afa95ee94ccce2e4653038122dde0de36f
Jonathan Hop <yithril@gmail.com>
2024-07-12 11:40:57 -0400

    front end

--- Week13-JDBC/4.Thursday/recipe-api/src/main/java/com/example/recipe_api/repositories/RecipeRepository.java
+++ Week13-JDBC/4.Thursday/recipe-api/src/main/java/com/example/recipe_api/repositories/RecipeRepository.java
@@ -4,6 +4,11 @@ import com.example.recipe_api.models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    //derived queries
+    List<Recipe> findByNameContainingIgnoreCase(String name);
+    List<Recipe> findByCookingTimeInMinutesBetween(int start, int end);
 }
