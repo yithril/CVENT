7280b40a47e3b02dc884652ad1985e2aa3178971
Jonathan Hop <yithril@gmail.com>
2024-07-17 12:01:30 -0400

    sync and async

--- Week14-SpringBoot/3.Wednesday/SyncVsAsync/src/main/java/org/example/Main.java
+++ Week14-SpringBoot/3.Wednesday/SyncVsAsync/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //How to use completable future
+        //does it return something or does it not return something?
+
+        //We expect some data back from our process
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+           try{
+               Thread.sleep(1000);
+           }
+           catch(InterruptedException ex){
+               ex.printStackTrace();
+           }
+           return "Hello this is supply async!";
+        });
+
+        //What if there is no data to give back?
+        CompletableFuture<Void> voidFuture = CompletableFuture.runAsync(() -> {
+            System.out.println("Run async!");
+        });
     }
 }
