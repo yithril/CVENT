3e8863595681004e8bd1c18be2cf116245b7e627
Jonathan Hop <yithril@gmail.com>
2024-07-23 10:00:58 -0400

    react testing and typescript

---
+++ Week15-Testing/2.Tuesday/my-react-app/jest.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+  };
+  
